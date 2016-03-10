36817baf3aa1b63659c1abf64e72f9cc22f9cc50
Samuel Steele <steelesam72@gmail.com>
2016-03-10 15:38:34 -0500

    Edited `/api/v1/compliment`

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var express = require('express'),
     app = express(),
     exphbs = require('express-handlebars'),
     favicon = require('serve-favicon'),
-    cameron = require('./js/app/cameron.js')
+    cameron = require('./js/app/cameron.js'),
+    cameronNormal = require('./public/scripts/script.js')
 
-const comp = require('./js/app/comp.js')
+var comp = require('./js/app/comp.js')
 
 String.prototype.capitalizeFirstCharacter = function() {
     return this.charAt(0).toUpperCase() + this.slice(1)
@@ -13,19 +14,27 @@ String.prototype.capitalizeFirstCharacter = function() {
 app.use(express.static('public'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
-app.engine('handlebars', exphbs({defaultLayout: 'default'}));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'default'
+}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
-    res.render('index', {home: true});
+    res.render('index', {
+        home: true
+    });
 });
 
 app.get('/about', function(req, res) {
-    res.render('about', {about: true});
+    res.render('about', {
+        about: true
+    });
 });
 
 app.get('/docs', function(req, res) {
-    res.render('docs', {docs: true})
+    res.render('docs', {
+        docs: true
+    })
 });
 
 app.get('/compliment', function(req, res) {
@@ -36,7 +45,9 @@ app.get('/compliment', function(req, res) {
             compliment = compliment.capitalizeFirstCharacter()
         }
         if (req.query.format === "json") {
-            res.json({compliment: compliment})
+            res.json({
+                compliment: compliment
+            })
         } else {
             res.render('compliment', {
                 compliment: compliment
@@ -50,7 +61,9 @@ app.get('/compliment/:name', function(req, res) {
         var name = req.params.name
         compliment = name.capitalizeFirstCharacter() + ", " + compliment
         if (req.query.format === "json") {
-            res.json({compliment: compliment})
+            res.json({
+                compliment: compliment
+            })
         } else {
             res.render('compliment', {
                 compliment: compliment
@@ -69,12 +82,27 @@ app.get('/compliment/markov', function(req, res) {
 });
 
 app.get('/api/v1/compliment', function(req, res) {
-    cameron(function(compliment) {
-        res.send(compliment)
-    })
+    if (req.query.type) {
+        var name = ""
+        if (req.query.name) {
+            name = req.query.name + ", "
+        }
+        if (req.query.type.toLowerCase() == "markov") {
+            cameron(function(compliment) {
+                res.send((name + compliment).capitalizeFirstCharacter())
+            })
+        } else if (req.query.type.toLowerCase() == "normal") {
+            res.send((name + cameronNormal()).capitalizeFirstCharacter())
+        } else {
+            res.send("Unkown <em>type</em> value")
+        }
+    } else {
+        res.send("Must use query operator ?type=\<<em>markov</em>|<em>normal</em>\>")
+    }
 })
 
 app.get('/testing', function(req, res) {
+    // This endpoint is for testing code
     comp(function(compliment) {
         res.send(compliment)
     })
